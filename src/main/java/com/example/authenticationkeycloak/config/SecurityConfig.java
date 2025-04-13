7e4bb358b6ded21f56aa5402fd292d1d9a5899d9
LucaTurco99 <lucaturco1999@gmail.com>
2025-04-13 15:11:08 +0200

    init keycloak base setup

---
+++ src/main/java/com/example/authenticationkeycloak/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.authenticationkeycloak.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.decoder(jwtDecoder()))
+                );
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(issuer);
+    }
+}
---
+++ src/test/java/com/example/authenticationkeycloak/AuthenticationKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticationkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
